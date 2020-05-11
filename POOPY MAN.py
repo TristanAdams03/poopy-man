d22220fa16a8f61aa10636dd0aa3079f596ec6db
TristanAdams03 <65198617+TristanAdams03@users.noreply.github.com>
2020-05-11 17:48:02 -0500

    Add files via upload

---
+++ POOPY MAN.py
@@ -0,0 +1,70 @@
+import paho.mqtt.client as mqtt
+import matplotlib.pyplot as plt
+import time
+
+a = []
+b = []
+c = []
+d = []
+e = []
+f = []
+g = []
+h = []
+i = []
+j = []
+k = []
+end = False
+biglist = [a, b, c, d, e, f, g, h, i, j, k]
+active = 0
+
+def on_message(client, userdata, message):
+    global active
+    global biglist
+    global end
+    
+    data = str(message.payload.decode("utf-8"))
+    if 'BEGIN' in data:
+        pass
+    elif 'END' in data:
+        end = True    
+    elif 'SHAPE' in data:
+        print("New Shape: ", data)
+        active += 1
+    else:
+        biglist[active].append(data)
+    print(data)
+
+
+
+def draw_shape(submitted_shape):
+    x = []
+    y = []
+    for pair in submitted_shape:
+        if '#' in pair:
+            color = pair
+            plt.plot(x,y,color= color)
+        elif 'SHAPE' in pair:
+            pass
+        else:
+            x.append(float(pair.split(",")[0]))
+            y.append(float(pair.split(",")[1]))
+
+    
+
+broker = "34.67.109.247"
+port = 13371
+
+client = mqtt.Client('Sumting')
+client.on_message = on_message
+client.connect(broker, port)
+
+
+while end != True:
+    client.loop_start()
+    client.subscribe("eoc/pic")
+    client.loop_stop() #stop the loop
+print(biglist)
+for shape in biglist:
+    draw_shape(shape)
+
+plt.show()
